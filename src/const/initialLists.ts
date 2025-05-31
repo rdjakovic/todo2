9f724282fddf3915556c6569dea20b3400bddd3b
Ranko <d.ranko@gmail.com>
2025-05-31 09:39:16 +0200

    Updated initialLists.ts

--- src/const/initialLists.ts
+++ src/const/initialLists.ts
@@ -6,27 +6,27 @@ export const initialLists: TodoList[] = [
     name: "Home",
     icon: "home",
     todos: [],
-    showCompleted: true, // maps to is_completed_hidden: false in database
+    showCompleted: true, 
   },
   {
     id: 2,
     name: "Completed",
     icon: "check",
     todos: [],
-    showCompleted: true, // maps to is_completed_hidden: false in database
+    showCompleted: true, 
   },
   {
     id: 3,
     name: "Personal",
     icon: "user",
     todos: [],
-    showCompleted: true, // maps to is_completed_hidden: false in database
+    showCompleted: true, 
   },
   {
     id: 4,
     name: "Work",
     icon: "briefcase",
     todos: [],
-    showCompleted: true, // maps to is_completed_hidden: false in database
+    showCompleted: true, 
   },
 ];
