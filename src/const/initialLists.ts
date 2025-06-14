486c24dc655daec54887388c3e0e30be3bdf54ae
Ranko <d.ranko@gmail.com>
2025-06-14 13:45:52 +0200

    Updated initialLists.ts

--- src/const/initialLists.ts
+++ src/const/initialLists.ts
@@ -2,25 +2,22 @@ import { TodoList } from "../types/todo";
 
 export const initialLists: Partial<TodoList>[] = [
   {
-    id: "all",
     name: "All",
     icon: "home",
     showCompleted: true,
   },
   {
-    id: "completed",
+    id: "completed"
     name: "Completed",
     icon: "check",
     showCompleted: true,
   },
   {
-    id: "personal",
     name: "Personal",
     icon: "user",
     showCompleted: true,
   },
   {
-    id: "work",
     name: "Work",
     icon: "briefcase",
     showCompleted: true,
